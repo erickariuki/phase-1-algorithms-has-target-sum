e59518f644edd38f976150a9a85139a282b9b628
erickariuki <eric.kariuki@student.moringaschool.com>
2023-04-01 19:08:47 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 function hasTargetSum(array, target) {
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
   // Write your algorithm here
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
